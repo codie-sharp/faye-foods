a4245db66294b6a49387b29c52e18cd129988761
Codie <codie.sharp@gmail.com>
2025-02-25 16:19:53 -0500

    Going to have to use rays after all

--- MainScript.gd
+++ MainScript.gd
@@ -2,10 +2,13 @@ extends Node3D
 
 var available_seats = []
 var pantry = []
+var camera
 var selected_food
 var is_dragging = false
 
 func _ready():
+	camera = get_viewport().get_camera_3d()
+	
 	pantry = get_node("Counter/Pantry").get_children()
 	for food in pantry:
 		food.food_selected.connect(_on_food_selected)
@@ -24,8 +27,10 @@ func _input(event):
 	if event is InputEventMouseButton and !event.pressed:
 		is_dragging = false
 
+	# Get used to rays big dog
 	#elif is_dragging and event is InputEventMouseMotion:
-		## update dragged mesh here
+		#var world_pos = camera.project_position(event.position, 0)
+		#selected_food.global_transform.origin = Vector3(world_pos.x, world_pos.y, 0)
 
 func _on_food_selected(food):
 	is_dragging = true
--- PlateScript.gd
+++ PlateScript.gd
@@ -6,10 +6,16 @@ var state = false
 
 func _ready():
 	connect("input_event", _on_input_event)
+	connect("area_entered", _on_area_entered)
 
 func _on_input_event(_camera, event, _click_position, _click_normal, _shape_idx):
+	# Body entered?
 	if state and event is InputEventMouseButton and event.pressed:
 		emit_signal("food_plated")
 
 func change_state():
 	state = !state
+
+func _on_area_entered():
+	if state:
+		print("area_entered")
